d830838b68264540e86bbbb5936cc6aef3e8cf07
colinhannah2000 <colinshannah@gmail.com>
2015-11-10 17:40:20 +0530

    Add C# OMS

---
+++ CSharp/OMS/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS
+{
+    public class Message : IMessage
+    {
+        public long Id { get; internal set; }
+    }
+}
---
+++ CSharp/OMS/OmsFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS
+{
+    public class OmsFactory : IOmsFactory
+    {
+        private long _nextId = 0;
+        public OmsFactory()
+        {
+
+        }
+
+        public IOrder CreateOrder()
+        {
+            Order order = CreateMessage<Order>();
+            return order;
+        }
+
+        public IExecutionReport CreateExectionReport()
+        {
+
+            ExecutionReport execution = CreateMessage<ExecutionReport>();
+            return execution;
+        }
+
+        private TMessage CreateMessage<TMessage>() where TMessage : IMessage, new()
+        {
+           return new TMessage() { Id = _nextId++};
+        }
+    }
+}
