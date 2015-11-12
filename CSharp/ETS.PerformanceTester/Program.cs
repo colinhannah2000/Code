3da7cc8f3fa2e549880b4c56a481ebc0aa471171
colinhannah2000 <colinshannah@gmail.com>
2015-11-12 15:03:55 +0530

    Add performance tester.

---
+++ CSharp/ETS.PerformanceTester/Program.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using ETS.Configuration;
+using System.Configuration;
+using ETS.OMS;
+
+namespace ETS.PerformanceTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            Configuration.Configuration.Build(builder, ConfigurationManager.AppSettings["Markets"]);
+
+            builder.RegisterType<SingleSimpleMatcher>().As<IMatcher>();
+            builder.RegisterType<OmsFactory>().As<IOmsFactory>();
+
+            IContainer container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var configuration = scope.Resolve<IConfiguration>();
+                var orders = scope.Resolve<IOmsFactory>();
+                var matcher = scope.Resolve<IMatcher>();
+
+                TestMatcher(orders, matcher);
+            }
+        }
+
+        private static void TestMatcher(IOmsFactory orders, IMatcher matcher)
+        {
+            var trades1 = matcher.AddOrder(orders.CreateOrder(10,Side.Buy,100));
+            var trades2 = matcher.AddOrder(orders.CreateOrder(10, Side.Sell, 100));
+        }
+    }
+}
